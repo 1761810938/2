1c7eaebc4b194ba06bd114dabf781fd5b7fa68df
Xu.WANG <raymondmgwx@gmail.com>
2022-09-01 12:33:24 +0900

    update

--- HYCOM/r3.0/service-worker.js
+++ HYCOM/r3.0/service-worker.js
@@ -1,3 +1,3 @@
-importScripts("/HYCOM/r3.0/precache-manifest.ebfcbcc1bccbfaae40a2ace7cd4815d4.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/HYCOM/r3.0/precache-manifest.8c9aa5c6d9ce2539a6eade2161597cf8.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 
